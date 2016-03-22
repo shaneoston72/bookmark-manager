b5d4919f0dab407c38b565e54f30a1766ad1d8da
Shane Oston Stowe <shane@shaneoston.com>
2016-03-22 10:40:28 +0000

    added first test; finished challenge 8

---
+++ spec/features/vewing_links_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature 'viewing_links_spec: Bookmark Manager' do
+
+  scenario 'show links' do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy' )
+
+    visit '/' # used '/links'
+
+    expect(page.status_code).to eq 200
+
+    within('ul') do #used ul#links
+      expect(page).to have_content('Makers Academy')
+    end
+  end
+end
